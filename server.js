7ee911f3b3d12a2cf2186c0b0bf66be34d77ba11
SenpaiVR <SenpaiShichi@gmail.com>
2024-06-21 23:12:10 -0400

    Auto Update

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const fs = require('fs')
 const { exec, spawn } = require('child_process')
 
-console.log('Watching for file changes')
-let GithubTimeout = null
-function Watch() {
-	const watcher = fs.watch('./', (evt, file) => {
-		if (!file.endsWith('.json')) return
-		watcher.close()
-		clearTimeout(GithubTimeout)
-		if (GithubTimeout) clearTimeout(GithubTimeout)
-		GithubTimeout = setTimeout(() => {
-			UpdateGithub()
-		}, 5000)
-		setTimeout(Watch, 500)
-	})
-}
+exec('npm install', (_, stdout, __) => { // Install dependencies
+	
+	console.log('Watching for file changes')
+	let GithubTimeout = null
+	function Watch() {
+		const watcher = fs.watch('./', (evt, file) => {
+			if (!file.endsWith('.json')) return
+			watcher.close()
+			clearTimeout(GithubTimeout)
+			if (GithubTimeout) clearTimeout(GithubTimeout)
+			GithubTimeout = setTimeout(() => {
+				UpdateGithub()
+			}, 5000)
+			setTimeout(Watch, 500)
+		})
+	}
 
-function UpdateGithub() {
-	const cmd = 'git add . && git commit -m "Auto Update" && git push'
-	console.log(cmd)
-	// exec(cmd, (_, stdout, __) => {
-	// 	console.log(stdout)
-	// })
-}
+	function UpdateGithub() {
+		const cmd = 'git add . && git commit -m "Auto Update" && git push'
+		console.log(cmd)
+		// exec(cmd, (_, stdout, __) => {
+		// 	console.log(stdout)
+		// })
+	}
 
-Watch()
+	Watch()
+})
