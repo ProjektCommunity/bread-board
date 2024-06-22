cfbaa77767f80f104f51fea28b4cbbd85066ed58
SenpaiVR <SenpaiShichi@gmail.com>
2024-06-21 22:36:32 -0400

    Auto Update

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const fs = require('fs')
 const { exec } = require('child_process')
 
 console.log('Watching for file changes')
+let GithubTimeout = null
 function Watch() {
 	const watcher = fs.watch('./', (evt, file) => {
 		if (!file.endsWith('.json' || '.js')) return
 		watcher.close()
+		clearTimeout(GithubTimeout)
 		console.log('File Change Detected', new Date(), evt, file)
-		setTimeout(UpdateGithub, 1000)
+		if (GithubTimeout) clearTimeout(GithubTimeout)
+		GithubTimeout = setTimeout(() => {
+			console.log('Updating Github')
+			UpdateGithub()
+		}, 1000)
 		setTimeout(Watch, 500)
 	})
 }
